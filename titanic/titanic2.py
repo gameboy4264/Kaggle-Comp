9c24b43d5c4ba8d6b5558c23ca0420682e752964
gameboy4264 <gameboy4264@163.com>
2019-04-14 18:58:57 -0500

    data process

--- titanic/titanic2.py
+++ titanic/titanic2.py
@@ -74,3 +74,18 @@ output = pd.DataFrame({
 output.to_csv('submission.csv')
 
 # 8 预测结果提交
+
+
+
+"""
+PassengerId => 乘客ID
+Pclass => 乘客等级(1/2/3等舱位)
+Name => 乘客姓名
+Sex => 性别
+Age => 年龄
+SibSp => 堂兄弟/妹个数
+Parch => 父母与小孩个数
+Ticket => 船票信息
+Fare => 票价
+Cabin => 客舱
+Embarked => 登船港口
